002e00761d843a126337abac57cd88d413e6d7b0
AlexanderNissen <77581312+AlexanderNissen@users.noreply.github.com>
2023-03-09 20:44:49 +0100

    Initial commit

---
+++ src/store/index.js
@@ -0,0 +1,35 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const songsSlice = createSlice({
+  name: "song",
+  initialState: [],
+  reducers: {
+    addSong(state, action) {
+      state.push(action.payload);
+    },
+    removeSong(state, action) {
+      //
+    }
+  }
+});
+
+const store = configureStore({
+  reducer: {
+    songs: songsSlice.reducer
+  }
+});
+
+store.dispatch(songsSlice.actions.addSong("Some song"));
+
+console.log(store.getState(songsSlice.name));
+
+// const startingState = store.getState();
+// console.log(JSON.stringify(startingState));
+
+// store.dispatch({
+//   type: "song/addSong",
+//   payload: "New song!!!"
+// });
+
+// const finalState = store.getState();
+// console.log(JSON.stringify(finalState));
